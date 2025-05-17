d72bc6da9ee276024a9d06df70f421153aa74693
Andrii <pylypchukandrii770@gmail.com>
2025-05-17 21:49:18 +0200

    Resolve merge conflict in run.py

--- run.py
+++ run.py
@@ -4,3 +4,10 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run()
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+    
+=======
+>>>>>>> 5a02bce (Move Procfile and run.py to project root for correct Render deployment)
+>>>>>>> ee13604 (Restore project structure and add Procfile/run.py to root)
